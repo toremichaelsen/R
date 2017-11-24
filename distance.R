fc0ea04d32b27ecef1c7ca7bb7206af093d546ff
toremichaelsen <33689862+toremichaelsen@users.noreply.github.com>
2017-11-24 20:01:08 +0100

    Delete distance.R

--- distance.R
+++
@@ -1,8 +0,0 @@
-#' Calculates distance to a sound
-#' Uses a reference dB value at a reference distance to calculate the distance
-#' @export
-distance <- function(L2, L1,r1) {
-  distance<-r1*10^((L1-L2)/20)
-  dB<-L2
-  return(data.frame(dB=dB,distance=distance))
-}
